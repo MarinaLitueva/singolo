870053ec207ed9cb801eb0428911b08e64436d79
Marina <lituevam@gmail.com>
2020-03-21 22:16:46 +0300

    refactor: change structure of the project and add new feature

--- script.js
+++ script.js
@@ -8,44 +8,116 @@ menu.addEventListener('click', (event)=>{
     event.target.classList.add('nav__link_active') 
 })
 
-//Переключение слайдер
 
-/* Индекс слайда по умолчанию */
-var slideIndex = 1;
-showSlides(slideIndex);
 
-/* Функция увеличивает индекс на 1, показывает следующй слайд*/
-function plusSlide() {
-    showSlides(slideIndex += 1);
+document.addEventListener ('scroll', onScroll)
+
+function onScroll (event) {
+ var curPos= window.scrollY;
+ var section = document.querySelectorAll ('#main>section');
+ var links = document.querySelectorAll('#list a');
+ section.forEach((el)=>{
+
+    if (el.offsetTop<=curPos+95 && (el.offsetTop + el.offsetHeight)>curPos+95) {
+        links.forEach((a)=> {
+            a.classList.remove('nav__link_active')           
+            
+            if(el.dataset.id===a.getAttribute('href').substring(1)) {
+                a.classList.add('nav__link_active')
+            }
+        })
+    } 
+    if (curPos > (section[section.length - 1].offsetTop - 500)) {
+        links[links.length - 2].classList.remove('nav__link_active')
+        links[links.length - 1].classList.add('nav__link_active')
+	}
+ });  
+
 }
 
-/* Функция уменьшяет индекс на 1, показывает предыдущий слайд*/
-function minusSlide() {
-    showSlides(slideIndex -= 1);  
+/* Slider*/
+
+let SLIDER = document.getElementById('slider');
+let slides = document.querySelectorAll('.slide_single');
+let current = 0;
+let sliderBlocked = false;
+
+
+function slider_init() {
+    let offset = 0;
+    let slide2 = (current === 0) ? 1 : 0;
+    SLIDER.innerHTML = '';
+    let elem = slides[slide2].cloneNode(true);
+    elem.style.left = offset*830 - 830 + 'px';
+    slides[current].style.left = offset*830 + 'px';
+    offset += 1;
+    slides[slide2].style.left = offset*830 + 'px';
+    SLIDER.appendChild(elem);
+    SLIDER.appendChild(slides[current]);
+    SLIDER.appendChild(slides[slide2]);
 }
 
-/* Устанавливает текущий слайд */
-function currentSlide(n) {
-    showSlides(slideIndex = n);
-}
-
-/* Основная функция слайдера */
-function showSlides(n) {
-    var i;
-    var slides = document.getElementsByClassName("item-img");
-    
-    if (n > slides.length) {
-      slideIndex = 1
-    }
-    if (n < 1) {
-        slideIndex = slides.length
-    }
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
+function slide_left() {
+    if (!sliderBlocked) {
+        sliderBlocked = true;
+        let slides2 = document.querySelectorAll('.slide_single');
+        let offset2 = -1;
+        for (let i = 0; i < slides2.length; i += 1) {
+            slides2[i].style.left = offset2*830 - 830 +'px';
+            offset2 += 1;
+        }
+        current += 1;
+        if (current >= slides.length) {
+            current = 0;
+        }
+        if (current === 1) {
+            document.getElementById('slider-wrapper').classList.add('bgBlue');
+        } else {
+            document.getElementById('slider-wrapper').classList.remove('bgBlue');
+        }
     }   
-    slides[slideIndex - 1].style.display = "block";    
 }
 
+function slide_right() {
+    if (!sliderBlocked) {
+        sliderBlocked = true;
+        let slides2 = document.querySelectorAll('.slide_single');
+        let offset2 = -1;
+        for (let i = 0; i < slides2.length; i += 1) {
+            slides2[i].style.left = offset2*830 + 830 +'px';
+            offset2 += 1;
+        }
+        current += 1;
+        if (current >= slides.length) {
+            current = 0;
+        }
+        if (current === 1) {
+            document.getElementById('slider-wrapper').classList.add('bgBlue');
+        } else {
+            document.getElementById('slider-wrapper').classList.remove('bgBlue');
+        }
+    } 
+}
+SLIDER.addEventListener('transitionend', function () {
+    slider_init();
+    sliderBlocked = false;
+});
+document.getElementById('arrow_left').addEventListener('click', slide_right);
+document.getElementById('arrow_right').addEventListener('click', slide_left);
+slider_init();
+
+/* Slider displays*/
+
+var display = document.querySelector('.iPhone_Vert')
+var displ = document.querySelector('.iPhone_Hor')
+
+display.addEventListener('click', event => {   
+    display.classList.toggle('display-off');       
+});
+
+displ.addEventListener('click', event => {   
+    displ.classList.toggle('display-off');       
+});
 
 //Смена картинок
 
@@ -83,15 +155,25 @@ btn.addEventListener('click', ()=>{
     var subject = document.getElementById('subject').value.toString();
     var describe = document.getElementById('describe').value.toString()
     var sub =document.getElementById('result');
-    sub.innerText="Тема:" + subject;    
+
+    if ((subject !='') || (describe !='')){
+        sub.textContent = 'Тема: ' + subject + describe
+    } 
+    else {
+        sub.textContent = 'Без темы'
+    };
+
     document.getElementById('PopUp').classList.remove('hidden') 
 })
  
 
 btnClose.addEventListener('click', ()=>{
-
     document.getElementById('result').innerText="";
     document.getElementById('PopUp').classList.add('hidden');
     document.getElementById('myForm').reset() 
 })
  
+
+
+
+
